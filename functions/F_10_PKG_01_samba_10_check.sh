f833c988b73da50fcafaa5a16aa035acfab09e9e
Charlie Hsieh <charlieofficial@gmail.com>
2020-07-07 22:24:24 +0800

    add msg for check script

--- functions/F_10_PKG_01_samba_10_check.sh
+++ functions/F_10_PKG_01_samba_10_check.sh
@@ -1,3 +1,8 @@
+# =====================
+# Enable databag
+# =====================
+# DATABAG_CFG:enable
+
 # --------------Load .bash_profile-------------
 # Load .bash_profile to activate samba commands
 . ${PLUGINS}/plugin_load_bash_profile.sh
@@ -50,3 +55,17 @@ testparm -s
 echo ""
 echo ""
 
+
+echo "--------------------------------------------"
+echo "You could now manage AD via RSAT"
+echo "https://github.com/charlietag/Samba4_AD_RSAT#manage-active-directory---dc-with-rsat-windows"
+echo "--------------------------------------------"
+echo ""
+echo ""
+echo "--------------------------------------------"
+echo "--------------------------------------------"
+echo "Before go on, maybe you could try command below to verify your samba-ad-dc status:"
+echo "    . ~/.bash_profile (if need)"
+echo ""
+echo "    smbclient //localhost/netlogon -UAdministrator -c 'ls'"
+echo "    (DEFAULT ADMIN PASSWORD: ${samba_admin_password})"
