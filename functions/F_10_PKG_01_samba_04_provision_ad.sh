ed7a7908f54571ec0393ac2316d59f5c426a6ee0
Charlie Hsieh <charlieofficial@gmail.com>
2020-07-07 13:40:26 +0800

    add load .bash_profile

--- functions/F_10_PKG_01_samba_04_provision_ad.sh
+++ functions/F_10_PKG_01_samba_04_provision_ad.sh
@@ -4,6 +4,12 @@
 # DATABAG_CFG:enable
 
 
+# --------------Load .bash_profile-------------
+# Load .bash_profile to activate samba commands
+. ${PLUGINS}/plugin_load_bash_profile.sh
+# --------------Load .bash_profile-------------
+
+
 samba-tool domain provision --server-role=${samba_server_role} --use-rfc2307 --dns-backend=${samba_dns_backend} --realm=${samba_realm} --domain=${samba_domain} --adminpass=${samba_admin_password}
 
 
--- functions/F_10_PKG_01_samba_05_setup.sh
+++ functions/F_10_PKG_01_samba_05_setup.sh
@@ -1,3 +1,8 @@
+
+# --------------Load .bash_profile-------------
+# Load .bash_profile to activate samba commands
 . ${PLUGINS}/plugin_load_bash_profile.sh
+# --------------Load .bash_profile-------------
+
 samba-tool -h
 #echo "test haha"
