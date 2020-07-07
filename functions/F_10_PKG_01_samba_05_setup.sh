26aa38c03570409a2b7399724ec3d272167ed4ba
Charlie Hsieh <charlieofficial@gmail.com>
2020-07-07 16:33:58 +0800

    add option to disable shared folder

--- functions/F_10_PKG_01_samba_05_setup.sh
+++ functions/F_10_PKG_01_samba_05_setup.sh
@@ -16,10 +16,19 @@ if [[ -n "${samba_config_file}" ]]; then
   sed -re '/dns\s+forwarder/d' -i ${samba_config_file}
   sed -e "/^\[global\]/a \ \ dns forwarder = ${samba_dns_forwarder}" -i  ${samba_config_file}
 
+  # ----------- Shared Folder ------------
+  if [[ -n "${samba_share_folder}" ]]; then
 
-  sed -re '/include/d' -i ${samba_config_file}
-  echo "include=/usr/local/samba/etc/shared.conf" >> ${samba_config_file}
+    sed -re '/include/d' -i ${samba_config_file}
+    echo "include=/usr/local/samba/etc/shared.conf" >> ${samba_config_file}
+    task_copy_using_render_sed
 
-  [[ -d "${samba_share_folder}" ]] || mkdir -p ${samba_share_folder}
-  task_copy_using_render_sed
+
+    if [[ ! -d "${samba_share_folder}" ]]; then
+      mkdir -p ${samba_share_folder}
+      chmod -R 777 ${samba_share_folder}
+    fi
+    
+  fi
+  # ----------- Shared Folder ------------
 fi
